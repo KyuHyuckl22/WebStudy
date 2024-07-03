74d7d9abb45012dbaee54fd29cbeea135a1cec09
user <user@DESKTOP-GD2QDUE>
2024-07-04 08:54:52 +0900

    240704

--- HTMLCSSProject_6/src/main/java/com/sist/database/DataBaseConnection.java
+++ HTMLCSSProject_6/src/main/java/com/sist/database/DataBaseConnection.java
@@ -1,31 +1,34 @@
 package com.sist.database;
+
 import java.sql.*;
 
 public class DataBaseConnection {
-	private Connection conn;
-	private final String URL="jdbc:oracle:@localhost:1521:XE";
-	
-	public DataBaseConnection() {
-		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-		}catch(Exception ex) {}
-	}
-	public Connection getConnection() {
-		try {
-			conn=DriverManager.getConnection(URL,"hr","happy");
-			
-		}catch(Exception ex) {}
-		return conn;
-	}
-	public void disConnection(Connection conn, PreparedStatement ps) {
-		try {
-			if (ps!=null)ps.close();
-			if (conn!=null) conn.close();
-			
-		}catch(Exception ex) {}
-	}
-	
-	
-	
-	
+   private Connection conn;
+   private final String URL = "jdbc:oracle:thin:@localhost:1521:XE";
+   
+   public DataBaseConnection() {
+      try {
+         Class.forName("oracle.jdbc.driver.OracleDriver");
+      } catch (Exception ex) {
+         ex.printStackTrace();
+      }
+   }
+   
+   public Connection getConnection() {
+      try {
+         conn = DriverManager.getConnection(URL, "hr", "happy");
+      } catch (Exception ex) {
+         ex.printStackTrace();
+      }
+      return conn;
+   }
+   
+   public void disConnection(Connection conn, PreparedStatement ps) {
+      try {
+         if (ps != null) ps.close();
+         if (conn != null) conn.close();
+      } catch(Exception ex) {
+         ex.printStackTrace();
+      }
+   }   
 }
