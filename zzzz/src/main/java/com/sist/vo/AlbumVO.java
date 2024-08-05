d6a2723f18e658fa4d960f43418a6fbb318da64d
user <user@DESKTOP-GD2QDUE>
2024-08-05 20:20:42 +0900

    rrrrrrr

---
+++ zzzz/src/main/java/com/sist/vo/AlbumVO.java
@@ -0,0 +1,29 @@
+package com.sist.vo;
+
+import java.util.Date;
+
+import lombok.Data;
+
+/*
+
+앨범스토어       album_store
+
+ANO			NUMBER						앨범번호
+APHOTO		VARCHAR2(2000 BYTE)			앨범사진
+ANAME			VARCHAR2(2000 BYTE)			앨범이름
+ARTIST			VARCHAR2(2000 BYTE)			아티스트
+ARDATE			DATE							발매일
+ARATING		NUMBER						별점
+APRICE			VARCHAR2(2000 BYTE)			앨범가격
+ADIS			VARCHAR2(2000 BYTE)			할인율
+ADISPRICE		VARCHAR2(2000 BYTE)			할인가격
+ADETAIL		VARCHAR2(4000 BYTE)			앨범속성
+AIMG			VARCHAR2(4000 BYTE)			앨범이미지
+
+ */
+@Data
+public class AlbumVO {
+	private int ano,arating;
+	private String aphoto,aname,artist,aprice,adis,adisprice,adetail,aimg;
+	private Date ardate;
+}
---
+++ zzzz/src/main/java/크롤링.java
@@ -0,0 +1,11 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import com.sist.vo.*;
+import com.sist.dao.*;
+public class 크롤링 {
+	public static void main(String[] args) {
+		
+	}
+}
+
